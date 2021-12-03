66076db899b3815379be8815391856e4d05e5035
UongHoangAnh45 <88814845+BanhMat45@users.noreply.github.com>
2021-12-03 14:36:46 +0700

    up code z

--- js/script.js
+++ js/script.js
@@ -34,6 +34,21 @@ $(document).ready(function () {
         autoplayHoverPause: true
     });
 
+    //slider product-box__list
+    $('#main_product-box').owlCarousel({
+        loop: true,
+        items: 3,
+        nav: true,
+        dots: false,
+    });
+
+    //slider simply-coffee
+    $('#simply-coffee').owlCarousel({
+        loop: true,
+        items: 1,
+        nav: false,
+        dots: true,
+    });
 
     // validate form liên hệ
     $('#commentForm').validate({
@@ -73,9 +88,36 @@ $(document).ready(function () {
     let searchObjec = $('.search a');
     let searchItem = $('.search-item');
     searchItem.hide();
-    searchObjec.on('click',function(){
-        if(searchObjec.length>0){
+    searchObjec.on('click', function () {
+        if (searchObjec.length > 0) {
             searchItem.toggle();
         }
     });
+
+    // ẩn hiển cho nút .why-us--reamore
+    $('.content_why-us').hide();
+    $('.why-us--reamore').click(function () {
+        if (!$('.content_why-us').hasClass('show')) {
+            $('.content_why-us').addClass('show');
+            $('.content_why-us').show();
+            $('.why-us--reamore button').text('Ẩn bớt');
+        } else {
+            $('.content_why-us').removeClass('show');
+            $('.content_why-us').hide();
+            $('.why-us--reamore button').text('Xem thêm');
+        }
+    });
+
+    // ẩn hiển cho nút .location-box
+    $('.location-box__item p').eq(1).hide();
+    $('.more').click(function () {
+        if (!$('.location-box__item p').eq(1).hasClass('show')) {
+            $('.location-box__item p').eq(1).addClass('show');
+            $('.location-box__item p').eq(1).show();
+        } else {
+            $('.location-box__item p').eq(1).removeClass('show');
+            $('.location-box__item p').eq(1).hide();
+        }
+    });
+
 });
